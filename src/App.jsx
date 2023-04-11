0c981d9de81f108267e62f6931dc3f499d8b2c5c
aayussraut <aayussraut.ar@gmail.com>
2023-04-11 16:45:46 +0545

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Header from "./components/Header";
-import SocialMedia from "./components/SocialMedia";
-import Button from "./components/Button";
-import ProjectCard from "./components/Project_Card";
-import SkillCard from "./components/SkillCard";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
 const App = () => {
   return (
     <div>
       {/* <h1>Hello World</h1> */}
       <Header />
-      <SocialMedia />
+      {/* <SocialMedia />
+      <br />
       <Button />
+      <br />
+      <br />
       <ProjectCard />
+      <br />
       <SkillCard />
+      <br />
+      <Form /> */}
+      <Home />
+      <About />
+      <Skills />
     </div>
   );
 };
