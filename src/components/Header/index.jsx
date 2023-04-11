5783bcea02b35497970e7c2c157a774c542c7071
aayussraut <aayussraut.ar@gmail.com>
2023-04-11 06:22:35 +0545

    Feature/Navbar

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../Logo";
+import Style from "./Header.module.css";
+import Menu from "../Menu";
+const Header = () => {
+  return (
+    <div className={`${Style.flex} ${Style.box}`}>
+      <Logo />
+      <Menu />
+      {/* <h1>Header</h1> */}
+    </div>
+  );
+};
+
+export default Header;
