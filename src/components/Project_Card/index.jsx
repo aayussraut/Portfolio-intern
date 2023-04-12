27f15ff74d9fbbbc793580e4188c7de2ab0423ef
aayussraut <aayussraut.ar@gmail.com>
2023-04-12 11:00:14 +0545

    Added Project Page

--- src/components/Project_Card/index.jsx
+++ src/components/Project_Card/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Style from "./ProjectCard.module.css";
-const ProjectCard = () => {
+const ProjectCard = ({ name, description, link }) => {
   return (
-    <div className={Style.card}>
-      <div className={Style.bar}></div>
-      <div className={Style.box}>
-        <div className={Style.header}>
-          <h2>Project Name</h2>
+    <div className={Style.container}>
+      <div className={Style.card}>
+        <div className={Style.bar}></div>
+        <div className={Style.box}>
+          <div className={Style.header}>
+            <h2>{name}</h2>
+          </div>
+          <p className={Style.grey}>{description}</p>
+          <a href={link} className={Style.link}>
+            Github
+          </a>
         </div>
-        <p className={Style.grey}>Project Description</p>
-        <a href="" className={Style.grey}>
-          Github
-        </a>
       </div>
     </div>
   );
---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Style from './Projects.module.css';
+import ProjectCard from '../../components/Project_Card';
+const Projects = () => {
+    return (
+        <div className={Style.project}>
+            <div>
+                <p className={Style.subtitle}>
+                    Here's My
+                </p >
+                <p className={Style.title}>
+                    Projects
+                </p>
+
+            </div>
+            <div className={Style.gridContainer}>
+
+                <ProjectCard name="KYC Sharing System" description="Decentralized App build using React,Blockchain,Node,Mongodb" link="https://github.com/aayussraut/Decentralized-KYC-Sharing-System.git" />
+                <ProjectCard name="Covid Update" description="Flutter application that fetch data from api and provides real time covid update" link="https://github.com/aayussraut/COVID-update.git" />
+                <ProjectCard name="Quiz App" description="Flutter application that fetch question from api" link="https://github.com/aayussraut/quizapp.git" />
+                <ProjectCard name="Simple User Mgmt System" description="Node and ejs application " link="https://github.com/aayussraut/user-management-system.git" />
+            </div>
+            {/* <h1>Projects</h1> */}
+        </div>
+    )
+}
+export default Projects;
