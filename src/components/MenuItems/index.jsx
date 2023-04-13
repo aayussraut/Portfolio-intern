8b411e36582a8f5379fb1c4409bc46c6c760f4c2
aayussraut <aayussraut.ar@gmail.com>
2023-04-13 14:07:50 +0545

    Added Hover Effect

--- src/components/MenuItems/index.jsx
+++ src/components/MenuItems/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Menu from "../Menu";
 import Style from "./MenuItems.module.css";
 
-const MenuItems = ({ menuItems }) => {
+const MenuItems = ({ menuItems, target_element }) => {
   return (
     <div className={`${Style.menu} ${Style.a} `}>
-      <a className={`${Style.link}`} href="">
+      <a className={`${Style.link}`} href={target_element}>
         {menuItems}
       </a>
     </div>
