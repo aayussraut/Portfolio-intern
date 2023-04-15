c07ed0eb0066ae2ac377dbb38560d9439358255e
aayussraut <aayussraut.ar@gmail.com>
2023-04-15 09:56:50 +0545

    Made Responsive and few changes

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AboutImg from "../../assets/AboutImg.svg";
+import AboutImg from "../../assets/LastImg.png";
 import Style from "./About.module.css";
 import { AboutPageCard, SocialMediaCard } from "../../components/Card";
 import Button from "../../components/Button";
@@ -12,27 +12,10 @@ import { AiFillGithub } from "react-icons/ai";
 const About = () => {
   return (
     <div id="About" className={Style.about}>
-      <div className={Style.content}>
-        <p className={Style.para}>Hi!</p>
-        <h3 className={Style.aboutHeader}>I am a Software Engineer</h3>
-        <p className={Style.para1}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
-          architecto reprehenderit minima debitis recusandae accusamus rerum
-          beatae inventore maxime, nam veritatis dolores repellat quidem error
-          odit voluptate neque illum ex.
-        </p>
-        <div className={Style.gridContainer}>
-          <AboutPageCard fieldName="Name" value="Aayush Raut" />
-          <AboutPageCard fieldName="Email" value="aayussraut.ar@gmail.com" />
-          <AboutPageCard fieldName="Phone" value="+9779861990090" />
-          <AboutPageCard fieldName="Address" value="Bhaktapur, Nepal" />
-        </div>
-        <div>
-          <Button name="My CV" />
-          <span style={{ margin: "10px" }}></span> <Button name="Hire Me" />
-        </div>
-      </div>
       <div className={Style.container}>
+        <div className={Style.imgDiv}>
+          <img src={AboutImg} alt="AboutImg" className={Style.aboutImg} />
+        </div>
         <div className={Style.socialMedia}>
           <SocialMediaCard
             link="https://www.facebook.com/aayussraut/"
@@ -51,8 +34,28 @@ const About = () => {
             icon={<TiSocialTwitter size={30} />}
           />
         </div>
-        <div className={Style.imgDiv}>
-          <img src={AboutImg} alt="AboutImg" className={Style.aboutImg} />
+      </div>
+
+      <div className={Style.content}>
+        <p className={Style.para}>Hi!</p>
+        <h3 className={Style.aboutHeader}>I am a Software Engineer</h3>
+        <p className={Style.para1}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          architecto reprehenderit minima debitis recusandae accusamus rerum
+          beatae inventore maxime, nam veritatis dolores repellat quidem error
+          odit voluptate neque illum ex.
+        </p>
+        <div className={Style.gridCon}>
+          <div className={Style.gridContainer}>
+            <AboutPageCard fieldName="Name" value="Aayush Raut" />
+            <AboutPageCard fieldName="Email" value="aayussraut.ar@gmail.com" />
+            <AboutPageCard fieldName="Phone" value="+9779861990090" />
+            <AboutPageCard fieldName="Address" value="Bhaktapur, Nepal" />
+          </div>
+        </div>
+        <div className={Style.twoButton}>
+          <Button name="My CV" />
+          <span style={{ margin: "10px" }}></span> <Button name="Hire Me" />
         </div>
       </div>
     </div>
