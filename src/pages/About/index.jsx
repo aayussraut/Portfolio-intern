67b99efdcd7badd788e3b45db9c673f44db8d592
aayussraut <aayussraut.ar@gmail.com>
2023-04-18 14:00:22 +0545

    Made Responsive and some adjustment

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -10,6 +10,43 @@ import {
 } from "react-icons/ti";
 import { AiFillGithub } from "react-icons/ai";
 const About = () => {
+  const socialMedia = [
+    {
+      link: "https://www.facebook.com/aayussraut/",
+      icon: <TiSocialFacebook size={30} />,
+    },
+    {
+      link: "https://www.linkedin.com/in/aayussraut/",
+      icon: <TiSocialLinkedin size={30} />,
+    },
+    {
+      link: "https://www.github.com/aayussraut/",
+      icon: <AiFillGithub size={30} />,
+    },
+    {
+      link: "https://www.twitter.com/aayussraut/",
+      icon: <TiSocialTwitter size={30} />,
+    },
+  ];
+
+  const aboutPageCard = [
+    {
+      title: "Name",
+      description: "Aayush Raut",
+    },
+    {
+      title: "Email",
+      description: "aayusraut.ar@gmail.com",
+    },
+    {
+      title: "Phone",
+      description: "+9779861990090",
+    },
+    {
+      title: "Address",
+      description: "Bhaktapur, Nepal",
+    },
+  ];
   return (
     <div id="About" className={Style.about}>
       <div className={Style.container}>
@@ -17,22 +54,9 @@ const About = () => {
           <img src={AboutImg} alt="AboutImg" className={Style.aboutImg} />
         </div>
         <div className={Style.socialMedia}>
-          <SocialMediaCard
-            link="https://www.facebook.com/aayussraut/"
-            icon={<TiSocialFacebook size={30} />}
-          />
-          <SocialMediaCard
-            link="https://www.linkedin.com/in/aayussraut/"
-            icon={<TiSocialLinkedin size={30} />}
-          />
-          <SocialMediaCard
-            link="https://www.github.com/aayussraut/"
-            icon={<AiFillGithub size={30} />}
-          />
-          <SocialMediaCard
-            link="https://www.twitter.com/aayussraut/"
-            icon={<TiSocialTwitter size={30} />}
-          />
+          {socialMedia.map((item) => {
+            return <SocialMediaCard link={item.link} icon={item.icon} />;
+          })}
         </div>
       </div>
 
@@ -47,10 +71,14 @@ const About = () => {
         </p>
         <div className={Style.gridCon}>
           <div className={Style.gridContainer}>
-            <AboutPageCard fieldName="Name" value="Aayush Raut" />
-            <AboutPageCard fieldName="Email" value="aayussraut.ar@gmail.com" />
-            <AboutPageCard fieldName="Phone" value="+9779861990090" />
-            <AboutPageCard fieldName="Address" value="Bhaktapur, Nepal" />
+            {aboutPageCard.map((item) => {
+              return (
+                <AboutPageCard
+                  title={item.title}
+                  description={item.description}
+                />
+              );
+            })}
           </div>
         </div>
         <div className={Style.twoButton}>
