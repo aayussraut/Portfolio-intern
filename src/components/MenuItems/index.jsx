aef2ad3ae06eb2e546f7340d85530aa394e1cfa4
aayussraut <aayussraut.ar@gmail.com>
2023-04-18 13:59:38 +0545

    Made Responsive and some adjustment

--- src/components/MenuItems/index.jsx
+++ src/components/MenuItems/index.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Menu from "../Menu";
 import Style from "./MenuItems.module.css";
 
-const MenuItems = ({ menuItems, target_element }) => {
+const DesktopMenuItems = ({ menuItem }) => {
+  // console.log("here11111");
   return (
     <div className={`${Style.menu} ${Style.a} `}>
-      <a className={`${Style.link}`} href={target_element}>
-        {menuItems}
+      <a className={`${Style.link}`} href={menuItem.target_element}>
+        {menuItem.name}
+      </a>
+    </div>
+  );
+};
+const MobileMenuItems = ({ menuItem, toggleMenu }) => {
+  return (
+    <div className={`${Style.menu} ${Style.a} `} onClick={toggleMenu}>
+      <a className={`${Style.link}`} href={menuItem.target_element}>
+        <div className={Style.icon}>{menuItem.icon}</div>
+        {menuItem.name}
       </a>
     </div>
   );
 };
 
-export default MenuItems;
+export { DesktopMenuItems, MobileMenuItems };
