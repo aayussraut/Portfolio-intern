56fde42a48cb2be31d10ef6f930b4e984b19bb17
aayussraut <aayussraut.ar@gmail.com>
2023-04-18 14:00:52 +0545

    Made Responsive and some adjustment

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Style from "./Skills.module.css";
 import { SkillCard } from "../../components/Card";
-
-import { FaReact, FaCss3, FaHtml5, FaNodeJs } from "react-icons/fa";
+import { FaHtml5, FaCss3, FaReact, FaNodeJs } from "react-icons/fa";
 import { DiJavascript1 } from "react-icons/di";
 const Skills = () => {
+  const skill = [
+    { skill: "HTML", icon: <FaHtml5 size={50} /> },
+    { skill: "CSS", icon: <FaCss3 size={50} /> },
+    { skill: "JavaScript", icon: <DiJavascript1 size={50} /> },
+    { skill: "React.js", icon: <FaReact size={50} /> },
+    { skill: "Node.js", icon: <FaNodeJs size={50} /> },
+  ];
   return (
     <div id="Skills" className={Style.skillPage}>
       <div>
@@ -15,11 +21,12 @@ const Skills = () => {
         </p>
       </div>
       <div className={`${Style.skillCard}  ${Style.item} `}>
-        <SkillCard skill="HTML" icon={<FaHtml5 size={50} />} />
-        <SkillCard skill="CSS" icon={<FaCss3 size={50} />} />
-        <SkillCard skill="JavaScript" icon={<DiJavascript1 size={50} />} />
-        <SkillCard skill="React.js" icon={<FaReact size={50} />} />
-        <SkillCard skill="Node.js" icon={<FaNodeJs size={50} />} />
+        {skill.map((item) => {
+          return (
+            <SkillCard skill={item.skill} icon={item.icon} />
+            // <h1>hi</h1>
+          );
+        })}
       </div>
     </div>
   );
