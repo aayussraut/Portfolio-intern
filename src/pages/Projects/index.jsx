719c98bc1d5983a351bd3368e2a842607eda7914
aayussraut <aayussraut.ar@gmail.com>
2023-04-18 14:00:42 +0545

    Made Responsive and some adjustment

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Style from "./Projects.module.css";
 import { ProjectCard } from "../../components/Card";
+import data from "../../data/data.json";
 const Projects = () => {
+  const [projectList, setProjectList] = useState([]);
+  useEffect(() => {
+    const projectData = data[0].projectList;
+    setProjectList(projectData);
+  }, []);
   return (
     <div id="Projects" className={Style.project}>
       <div>
@@ -9,26 +15,17 @@ const Projects = () => {
         <p className={Style.title}>Projects</p>
       </div>
       <div className={Style.gridContainer}>
-        <ProjectCard
-          name="KYC Sharing System"
-          description="Decentralized App build using React,Blockchain,Node,Mongodb"
-          link="https://github.com/aayussraut/Decentralized-KYC-Sharing-System.git"
-        />
-        <ProjectCard
-          name="Covid Update"
-          description="Flutter application that fetch data from api and provides real time covid update"
-          link="https://github.com/aayussraut/COVID-update.git"
-        />
-        <ProjectCard
-          name="Quiz App"
-          description="Flutter application that fetch question from api"
-          link="https://github.com/aayussraut/quizapp.git"
-        />
-        <ProjectCard
-          name="Simple User Mgmt System"
-          description="Node and ejs application "
-          link="https://github.com/aayussraut/user-management-system.git"
-        />
+        {projectList.map((item, index) => {
+          return (
+            <ProjectCard
+              key={index}
+              name={item.name}
+              description={item.description}
+              link={item.link}
+              image={item.image}
+            />
+          );
+        })}
       </div>
       {/* <h1>Projects</h1> */}
     </div>
