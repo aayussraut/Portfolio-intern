ade98298143aee4e75c2d210fe6e06c83a956941
aayussraut <aayussraut.ar@gmail.com>
2023-04-18 14:01:20 +0545

    Added Header in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import React from "react";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import Logo from "./components/Logo";
+import Menu from "./components/Menu";
+import "./index.css";
+const Header = () => {
+  const [viewPortWidth, setViewPortWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    const handleResize = () => setViewPortWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevValue) => !prevValue);
+  };
+
+  return (
+    <div className="Header">
+      <nav>
+        <Logo />
+        <Menu
+          isMenuOpen={isMenuOpen}
+          viewPortWidth={viewPortWidth}
+          toggleMenu={toggleMenu}
+        />
+        {/* <h1>Header</h1> */}
+      </nav>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
